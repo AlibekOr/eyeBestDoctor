b30c515b58278ccaa7da946918a5c96d219b0584
Ali <alibekorakbaev3132@gmail.com>
2023-09-16 19:23:45 +0500

    datajs

--- src/components/pages/section2/Section2.jsx
+++ src/components/pages/section2/Section2.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
-import Logo1 from "../../../util/images/sec2_png/1.jpg"
-import Logo2 from "../../../util/images/sec2_png/2.jpg"
-import Logo3 from "../../../util/images/sec2_png/3.jpg"
-import Logo4 from "../../../util/images/sec2_png/4.jpg"
-import Logo5 from "../../../util/images/sec2_png/5.jpg"
-import Logo6 from "../../../util/images/sec2_png/6.jpg"
+import { datajs } from './datajs'
+import { ArrowRightOutlined } from "@ant-design/icons"
 import "./section2.scss"
-const data = [
-    { id: '01', titile: 'Eye Exams', img: Logo1, dec: 'Quis risus sed vulputate odio ut. Vitae elementum curabitur vitae nunc velit' },
-    { id: '02', titile: 'Retina Repair', img: Logo2, dec: 'Tortor id aliquet lectus proin nibh nisl condimentum gravida rutrum quisque' },
-    { id: '03', titile: 'Dry Eye Surgery', img: Logo3, dec: 'Faucibus turpis in eu mi bibendum neque egestas congue in dictum non consectetur' },
-    { id: '04', titile: 'Laser Eye Surgery', img: Logo4, dec: 'Dapibus ultrices in iaculis nunc sed. Non diam phasellus vestibulum lorem sed risus ultricies' },
-    { id: '05', titile: 'Optical Service', img: Logo5, dec: 'Ultrices neque ornare aenean euismod elementum. Integer feugiat scelerisque varius' },
-    { id: '06', titile: 'Surgical Procedure', img: Logo6, dec: 'Massa enim nec dui nunc mattis enim ut tellus. Adipiscing diam donec adipiscing tristique' },
-]
+
 const Section2 = () => {
+    console.log(datajs);
     return (
         <div className='container'>
             <div className='sec2_titile'>
@@ -24,7 +14,7 @@ const Section2 = () => {
                 <span className='sec2_s'>Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo</span>
             </div>
             <div>
-                {data.map((item) => (
+                {datajs.map((item) => (
                     <div key={item.id}>
                         <div>
                             <div> {item.titile}</div>
@@ -32,6 +22,10 @@ const Section2 = () => {
                         </div>
                         <div> <img src={item.img} alt="foto" /> </div>
                         <div> {item.dec} </div>
+                        <div>
+                            <button>Read More</button>
+                            <ArrowRightOutlined />
+                        </div>
                     </div>
                 ))}
             </div>
