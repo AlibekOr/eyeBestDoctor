41166cd9b9e68154e99b0169bba71c6fe57f3a3e
Ali <alibekorakbaev3132@gmail.com>
2023-10-13 00:19:40 +0500

    section6 scss

--- src/components/pages/section6/Section6.jsx
+++ src/components/pages/section6/Section6.jsx
@@ -1,26 +1,39 @@
 import React from 'react'
 import { Carousel } from 'antd';
-
+import "./section6.scss"
 const Section6 = () => {
     return (
         <div className='sec2_block'>
-            <div className="container">
-                <div>section</div>
-                <div>
-                    <Carousel autoplay>
+            <div className="container ">
+                <div className="sect6">
+                    <div className='sect6__titile'>
+                        <div >Our Reviews</div>
                         <div>
-                            1
+                            Dapibus ultrices in iaculis nunc sed. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla
                         </div>
-                        <div>
-                            2
-                        </div>
-                        <div>
-                            3
-                        </div>
-                        <div>
-                            5
-                        </div>
-                    </Carousel>
+                    </div>
+                    <div className='carusel'>
+                        <Carousel className='carusel__inner' slidesPerRow={1} slidesToShow={3} dots autoplay>
+                            <div className='carusel__inner__div'>
+                                hello world 1
+                            </div>
+                            <div className='carusel__inner__div'>
+                                hello world 2
+                            </div>
+                            <div className='carusel__inner__div'>
+                                hello world 3
+                            </div>
+                            <div className='carusel__inner__div'>
+                                hello world   4
+                            </div>
+                            <div className='carusel__inner__div'>
+                                hello world 5
+                            </div>
+                            <div className='carusel__inner__div'>
+                                hello world 7
+                            </div>
+                        </Carousel>
+                    </div>
                 </div>
             </div>
         </div>
